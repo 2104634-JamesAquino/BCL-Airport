87188b4b74f03274464d32c2c16b1f4320f571fc
2104634-JamesAquino <2104634@brunel.ac.uk>
2022-02-14 09:34:51 +0000

    made finance warning screen

---
+++ src/finance/FinanceWarningScreen.java
@@ -0,0 +1,54 @@
+package finance;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JLabel;
+import java.awt.Font;
+
+public class FinanceWarningScreen extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FinanceWarningScreen frame = new FinanceWarningScreen();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FinanceWarningScreen() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 545, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNewButton = new JButton("OK");
+		btnNewButton.setFont(new Font("Tahoma", Font.BOLD, 10));
+		btnNewButton.setBounds(179, 184, 173, 37);
+		contentPane.add(btnNewButton);
+		
+		JLabel lblNewLabel = new JLabel("Record has one or more missing\n pieces of information");
+		lblNewLabel.setFont(new Font("Tahoma", Font.BOLD, 15));
+		lblNewLabel.setBounds(65, 64, 456, 87);
+		contentPane.add(lblNewLabel);
+	}
+}

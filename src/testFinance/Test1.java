9cec52fa0dab18a1b978a32bd36dcb885690d9c8
2104634-JamesAquino <2104634@brunel.ac.uk>
2022-02-17 10:18:56 +0000

    junit test attempt

--- src/testFinance/Test1.java
+++
@@ -1,7 +0,0 @@
-package testFinance;
-
-public class Test1 {
-	public static void main(String[] args) {
-		
-	}
-}
---
+++ src/testFinance/TestCase1.java
@@ -0,0 +1,14 @@
+package testFinance;
+import org.junit.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class TestCase1 {
+
+	@Test
+	public void testcalculateCompensation() {
+		assertEquals(expected: 100,testMethod.calculateCompensation(100));
+	}
+
+}

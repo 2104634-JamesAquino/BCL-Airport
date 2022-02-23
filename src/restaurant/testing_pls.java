7d1adeb2b4900e1ee109c80462516467967b712d
W4SIF <2109088@brunel.ac.uk>
2022-02-23 10:04:56 +0000

    Restuanrant code changed

---
+++ src/restaurant/testing_pls.java
@@ -0,0 +1,34 @@
+package restaurant;
+
+import java.util.HashMap;
+
+public class testing_pls {
+
+	public static void main(String[] args) {
+		HashMap<String , Integer> Restrant_data = new HashMap<String, Integer>();
+		
+		Restrant_data.put("Toast", 100);
+		Restrant_data.put("Sausages", 100);
+		Restrant_data.put("Pancakes", 100);
+		Restrant_data.put("Rice and Curry", 100);
+		Restrant_data.put("Fish and Chips", 100);
+		Restrant_data.put("Pasta", 100);
+		Restrant_data.put("CheeseCake", 100);
+		Restrant_data.put("Choclate Cake", 100);
+		Restrant_data.put("MilkShake", 100);
+		Restrant_data.put("Orange Juice", 100);
+		Restrant_data.put("Apple Juice", 100);
+		Restrant_data.put("Coffee", 1030);
+		
+		int Temp;
+		Temp = Restrant_data.get("Toast");
+		System.out.println(Temp);
+		Temp = Temp - 10;
+		System.out.println(Temp);
+		Restrant_data.put("Toast",Temp);
+		System.out.println(Restrant_data.get("Toast"));
+		
+		
+	}
+
+}
